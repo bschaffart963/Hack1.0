fbba0c704d3784e6b925494a23c7211d9fdf2606
bschaffart963 <42656545+bschaffart963@users.noreply.github.com>
2018-08-23 15:42:03 -0500

    Changes Made

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Brandon and Matthew Schaffart
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brandon Schaffart - Computer Science\n");
+
+  return 0;
+}
